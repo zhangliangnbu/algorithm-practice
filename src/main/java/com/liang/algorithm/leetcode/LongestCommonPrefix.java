ed4ce1953c915b31f3d6a86b53e1b9248f56e637
zhangliang <zhangliangnbu@gmail.com>
2020-06-15 18:00:38 +0800

    优化最长前缀算法

--- src/main/java/com/liang/algorithm/leetcode/LongestCommonPrefix.java
+++ src/main/java/com/liang/algorithm/leetcode/LongestCommonPrefix.java
@@ -25,9 +25,9 @@ package com.liang.algorithm.leetcode;
 public class LongestCommonPrefix {
     public static void main(String[] args) {
 //        String[] strs = {"flower","flow","flight"};
-        String[] strs = {"a"};
-//        String[] strs = {"dog","racecar","car"};
-        System.out.println(my2(strs));
+//        String[] strs = {"a"};
+        String[] strs = {"dog","racecar","car"};
+        System.out.println(offical(strs));
 
     }
 
@@ -111,6 +111,52 @@ public class LongestCommonPrefix {
         } else {
             return strs[shortestStrIndex].substring(0, index + 1);
         }
+    }
+
+    private static String offical(String[] strs) {
+        // 思路：状态转移，第n个字符串的前缀，即为n-1之前与n的比较
+        // time: O(mn)
+
+        if (strs.length == 0) {
+            return "";
+        }
+
+        if (strs.length == 1) {
+            return strs[0];
+        }
+
+        String prefix = strs[0];
+        for (int i = 1, len = strs.length; i < len; i ++) {
+            prefix = twoStrPrefix(prefix, strs[i]);
+            if (prefix.length() == 0) {
+                break;
+            }
+        }
+
+        return prefix;
+    }
+
+    private static String twoStrPrefix(String str1, String str2) {
+//        int index = -1;
+//        for (int i = 0, len = str1.length(); i < len; i ++) {
+//            if (str2.length() <= i || str2.charAt(i) != str1.charAt(i)) {
+//                break;
+//            }
+//            index = i;
+//        }
+//        if (index < 0) {
+//            return "";
+//        } else {
+//           return str1.substring(0, index + 1);
+//        }
+
+        int length = Math.min(str1.length(), str2.length());
+        int index = 0;
+        while (index < length && str1.charAt(index) == str2.charAt(index)) {
+            index++;
+        }
+        return str1.substring(0, index);
+
     }
 
     private static boolean isContainChar(String[] strs, char c, int i) {
