7cadc1b34cc0d3397d22ad59e62aa076ea2cdf75
zhangliang <zhangliangnbu@gmail.com>
2020-06-16 09:28:08 +0800

    优化有效括号

--- src/main/java/com/liang/algorithm/leetcode/BracketsValid.java
+++ src/main/java/com/liang/algorithm/leetcode/BracketsValid.java
@@ -1,5 +1,6 @@
 package com.liang.algorithm.leetcode;
 
+import java.util.HashMap;
 import java.util.Stack;
 
 /**
@@ -44,16 +45,22 @@ public class BracketsValid {
         String s4 = "([)]";
         String s5 = "{[]}";
 
-        System.out.println(isValid(s1));
-        System.out.println(isValid(s2));
-        System.out.println(isValid(s3));
-        System.out.println(isValid(s4));
-        System.out.println(isValid(s5));
+//        System.out.println(isValid(s1));
+//        System.out.println(isValid(s2));
+//        System.out.println(isValid(s3));
+//        System.out.println(isValid(s4));
+//        System.out.println(isValid(s5));
+
+        System.out.println(offical(s1));
+        System.out.println(offical(s2));
+        System.out.println(offical(s3));
+        System.out.println(offical(s4));
+        System.out.println(offical(s5));
 
     }
 
     public static boolean isValid(String s) {
-        // 思路：使用栈，遍历并与栈顶比较，配对则消掉，否则入栈
+        // 思路：使用栈，遍历并与栈顶比较，配对则出栈，否则入栈
         // time: O(n)
         Stack<Character> stack = new Stack<Character>();
         for (int i = 0, len = s.length(); i < len; i ++) {
@@ -72,6 +79,27 @@ public class BracketsValid {
             }
         }
 
+        return stack.isEmpty();
+    }
+
+    public static boolean offical(String s) {
+        // 思路：开括号入栈，闭括号与栈顶元素比较，配对则出栈，否则无效
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        Stack<Character> stack = new Stack<Character>();
+        for (int i = 0, len = s.length(); i < len; i ++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                if (stack.isEmpty() || stack.pop() != map.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
         return stack.isEmpty();
     }
 
